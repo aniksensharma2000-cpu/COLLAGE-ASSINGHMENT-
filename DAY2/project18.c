6d42a992546611665d72839ada3e3162f56ece21
aniksensharma2000-cpu <aniksensharma2000@gmail.com>
2026-08-23 21:44:34 +0530

    Update project18.c

--- DAY2/project18.c
+++ DAY2/project18.c
@@ -1,18 +1,9 @@
 #include <stdio.h>
-#include <math.h>
-
-int main()
-{
-    int a, b, c;
-    float r1, r2;
-
-    printf("Enter the values of a, b, c: \n");
-    scanf("%d%d%d", &a, &b, &c);
-
-    r1 = (- b - sqrt(b * b - 4 * a * c)) / 2 * a;
-    r2 = (- b + sqrt(b * b - 4 * a * c)) / 2 * a;
-
-    printf("Roots: %f, %f", r1, r2);
-
+int main (){
+    int sum = 0;
+    for(int i = 1; i <= 10;i++){
+        sum += i*i;
+    }
+    printf("The sum of the squares of the first 10 natural numbers is: %d\n", sum);
     return 0;
 }
